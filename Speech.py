d2c0a97bc869da1585ecb5854a2c97e220f5cc26
Jake Saphhire <yannick.mudinda@gmail.com>
2020-02-29 15:56:43 -0500

    Added punctuator support

--- Speech.py
+++ Speech.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import speech_recognition as sr
 import moviepy.editor as mp
@@ -12,11 +13,16 @@ r=sr.Recognizer()
 audio = sr.AudioFile("talkingaudio.wav")
 with audio as source:
     audio = r.record(source, duration=100)
-    apple=r.recognize_google(audio)
-    print(apple)
-
+    recognized_text=r.recognize_google(audio)
+    #For logging purposes
+    print(recognized_text)
+    import requests
+    data = {'text':recognized_text}
+    response = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
 
 f=open("Lesson.txt","w+")
-
-f.write(apple)
+if isinstance(response, basestring):
+    f.write(apple)
 f.close()
+
+
