436b01c1bf68b9bfc28bb0cf47e5b695bc98d188
Jake Saphhire <yannick.mudinda@gmail.com>
2020-02-29 17:31:33 -0500

    Added errror checking; Improved audio transcribing; Fixed requests

--- Speech.py
+++ Speech.py
@@ -7,23 +7,36 @@ import moviepy.editor as mp
 clip = mp.VideoFileClip("Talking.mp4")
 clip.audio.write_audiofile("talkingaudio.wav")
 
-
 r=sr.Recognizer()
 
-audio = sr.AudioFile("talkingaudio.wav")
-with audio as source:
-    audio = r.record(source, duration=100)
-    recognized_text=r.recognize_google(audio)
+audio_file = sr.AudioFile("talkingaudio.wav")
+with audio_file as source:
+    audio = r.record(source, duration=240)
+    try:
+        recognized_text=r.recognize_google(audio)
+    # Handles case for unrecognizable speech
+    except sr.UnknownValueError:
+        print("Google API could not recognize speech")
+        sys.exit(125)
+    except sr.RequestError:
+        print("Could not perform the request")
+        sys.exit(125)
     #For logging purposes
     print(recognized_text)
     import requests
     data = {'text':recognized_text}
     # Uses the following website to generate punctuated file
-    response = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
-
+    try:
+        response = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
+    except:
+        sys.exit(126)
+    # 200 Corresponds to HTML response code OK
+    if response.status_code != 200:
+        # Exits the program on erronous request
+        print("Incomplete request, Error code: " + response.status_code )
+        sys.exit(127)
+    #Logging purposes
+    print(response.text)
 f=open("Lesson.txt","w+")
-if isinstance(response, str):
-    f.write(response)
+f.write(response.text)
 f.close()
-
-
