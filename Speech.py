57df35402b503cb5e3b6816b468e71206a597e38
Kelbalooz <61632395+Kelbalooz@users.noreply.github.com>
2020-02-29 14:34:30 -0500

    Add files via upload
    Need to have speechrecognition and moviepy installed, video file in the same directory as code

---
+++ Speech.py
@@ -0,0 +1,22 @@
+import os
+import speech_recognition as sr
+import moviepy.editor as mp
+
+
+clip = mp.VideoFileClip("Talking.mp4")
+clip.audio.write_audiofile("talkingaudio.wav")
+
+
+r=sr.Recognizer()
+
+audio = sr.AudioFile("talkingaudio.wav")
+with audio as source:
+    audio = r.record(source, duration=100)
+    apple=r.recognize_google(audio)
+    print(apple)
+
+
+f=open("Lesson.txt","w+")
+
+f.write(apple)
+f.close()
