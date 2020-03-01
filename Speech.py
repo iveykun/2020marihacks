3c7c17da14a8108de1bd830c99c2b0a080be7a1d
Jake Saphhire <yannick.mudinda@gmail.com>
2020-02-29 20:00:35 -0500

    Added alternative speech recognition backend; Added optimization for short video files

--- Speech.py
+++ Speech.py
@@ -5,22 +5,41 @@ import speech_recognition as sr
 import moviepy.editor as mp
 
 clip = mp.VideoFileClip("Talking.mp4")
+length = clip.duration
 clip.audio.write_audiofile("talkingaudio.wav")
 
 r=sr.Recognizer()
-
+# Small file optimization
+# Skips the tentative Google API step for files
+# of known short length by setting the sphinx flag
+if length > 120:
+    sphinx = True
+else:
+    sphinx = False
 audio_file = sr.AudioFile("talkingaudio.wav")
 with audio_file as source:
-    audio = r.record(source, duration=240)
-    try:
-        recognized_text=r.recognize_google(audio)
-    # Handles case for unrecognizable speech
-    except sr.UnknownValueError:
-        print("Google API could not recognize speech")
-        sys.exit(125)
-    except sr.RequestError:
-        print("Could not perform the request")
-        sys.exit(125)
+    audio = r.record(source)
+    if not sphinx:
+        try:
+            recognized_text=r.recognize_google(audio)
+        # Handles case for unrecognizable speech
+        except sr.UnknownValueError:
+            print("Google API could not recognize speech")
+            sys.exit(125)
+        except sr.RequestError as error:
+            # Changing speech recognition back-end if Google refuses our request
+            print("Could not perform the request, Google Web-Speech API: {0}".format(error))
+            print("Switching to Sphinx...")
+            sphinx = True
+    if sphinx:
+        try:
+            recognized_text=r.recognize_sphinx(audio)
+        except sr.UnknownValueError:
+            print("Sphinx API failed to recognize speech")
+            sys.exit(125)
+        except sr.RequestError as error:
+            print("Sphinx API Error : {0}".format(error))
+            sys.exit(125)
     #For logging purposes
     print(recognized_text)
     import requests
