5109cdd76ccfd8ec796868656d8d923268ba902d
Jake Saphhire <yannick.mudinda@gmail.com>
2020-02-29 23:34:07 -0500

    Massive overhaul + code cleanup; Added audio-file splitting function to speech-recognizer; Implemented error-checking and perf. measurement

--- Speech.py
+++ Speech.py
@@ -3,23 +3,58 @@ import os
 import sys
 import speech_recognition as sr
 import moviepy.editor as mp
+import time
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
-clip = mp.VideoFileClip("Talking.mp4")
-length = clip.duration
-clip.audio.write_audiofile("talkingaudio.wav")
+def cut_audio(audiofile):
+    soundstream = AudioSegment.from_wav(audiofile)
+    # Splits wav file on silence
+    print("Splitting file")
+    chunks = split_on_silence(
+        soundstream, 
+        min_silence_len=400, 
+        # Silence threshold is deliberatly low
+        # since false positive have a much greater impact on sentence
+        # structure than false negatives
+        silence_thresh=-45, 
+    )
+    # Unable to find any silence
+    if chunks == []:
+        print("Unable to find silence!")
+        return [audiofile]
+    else:
+        chlen = len(chunks)
+    out_len = 100*1000
+    out_chunks = [chunks[0]]
+    i = 0 
+    for chunk in chunks[1:]:
+        print("Recombining " + str(i) + " on " + str(chlen))
+        if len(out_chunks[-1]) < out_len:
+            out_chunks[-1] += chunk
+        else:
+            out_chunks.append(chunk)
+        i += 1
+    out_list = []
+    i = 0
+    for newchunk in out_chunks:
+        #Adds silence to chunks
+        silence = AudioSegment.silent(duration=10)
+        audio_chunk = silence + newchunk + silence
 
-r=sr.Recognizer()
-# Small file optimization
-# Skips the tentative Google API step for files
-# of known short length by setting the sphinx flag
-if length > 120:
-    sphinx = True
-else:
-    sphinx = False
-audio_file = sr.AudioFile("talkingaudio.wav")
-with audio_file as source:
-    audio = r.record(source)
-    if not sphinx:
+        audio_chunk.export( "./chunk{0}.wav".format(i),
+                            bitrate = '192k', 
+                            format="wav")
+        filename = 'chunk'+str(i)+'.wav'
+        out_list.append(filename)
+        i += 1
+
+    return out_list
+
+
+def tts_from_file(source_file):
+    with sr.AudioFile(source_file) as source:
+        audio = r.record(source)
         try:
             recognized_text=r.recognize_google(audio)
         # Handles case for unrecognizable speech
@@ -29,33 +64,77 @@ with audio_file as source:
         except sr.RequestError as error:
             # Changing speech recognition back-end if Google refuses our request
             print("Could not perform the request, Google Web-Speech API: {0}".format(error))
-            print("Switching to Sphinx...")
-            sphinx = True
-    if sphinx:
-        try:
-            recognized_text=r.recognize_sphinx(audio)
-        except sr.UnknownValueError:
-            print("Sphinx API failed to recognize speech")
-            sys.exit(125)
-        except sr.RequestError as error:
-            print("Sphinx API Error : {0}".format(error))
-            sys.exit(125)
-    #For logging purposes
-    print(recognized_text)
-    import requests
-    data = {'text':recognized_text}
-    # Uses the following website to generate punctuated file
+            raise ValueError
+        #For logging purposes
+        # print(recognized_text)
+        if isinstance(recognized_text, str):
+            return recognized_text
+        else:
+            raise ValueError("Recognized Text not a string!")
+
+
+
+#######################
+#__   __       _
+#|  \/  | __ _(_)_ __
+#| |\/| |/ _` | | '_ \
+#| |  | | (_| | | | | |
+#|_|  |_|\__,_|_|_| |_|
+######################
+
+# constant strings to facilitate file interoperability
+wavfile = "tempwavaudio.wav"
+clip = mp.VideoFileClip("Talking.mp4")
+length = clip.duration
+clip.audio.write_audiofile(wavfile)
+
+r=sr.Recognizer()
+if length > 120:
+    need_cut = True
+else:
+    need_cut = False
+out_string = ""
+# Measuring TTS time
+start = time.time()
+if not need_cut:
     try:
-        response = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
-    except:
-        sys.exit(126)
-    # 200 Corresponds to HTML response code OK
-    if response.status_code != 200:
-        # Exits the program on erronous request
-        print("Incomplete request, Error code: " + response.status_code )
-        sys.exit(127)
-    #Logging purposes
-    print(response.text)
+        temp_string = tts_from_file(wavfile)
+        out_string += temp_string
+    except ValueError:
+        need_cut = True
+        # Resets output string
+        out_string = ""
+if need_cut:
+    filelist = cut_audio(wavfile)
+    maxlen = len(filelist)
+    index = 1
+    for audio_file in filelist:
+        print("Processing chunk " + str(index) + " of "  + str(maxlen))
+        try:
+            temp_string = tts_from_file(audio_file)
+            out_string += temp_string
+        except:
+            print("Skipped file " + audio_file + " in chunk list")
+        i+=1
+elapsed = start - time.time()
+#For logging purposes
+print(out_string + " in " + str(elapsed) " seconds")
+import requests
+data = {'text':out_string}
+# Uses the following website to generate punctuated file
+try:
+    response = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
+except:
+    sys.exit(126)
+# 200 Corresponds to HTML response code OK
+if response.status_code != 200:
+    # Exits the program on erronous request
+    print("Incomplete request, Error code: " + response.status_code )
+    sys.exit(127)
+#Logging purposes
+print(response.text)
 f=open("Lesson.txt","w+")
 f.write(response.text)
 f.close()
+
+
