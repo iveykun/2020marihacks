aa06dbc5d08010fe135749e3f6552d10a07957ef
Jake Saphhire <yannick.mudinda@gmail.com>
2020-03-01 12:26:24 -0500

    Code Reorganization; Speech.py kept untouched for posterity

--- TextRecTest.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-from PIL import Image
-
-image= input("Name of the image: ")
-img = Image.open(image)
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
-print( pytesseract.image_to_string(img) )
